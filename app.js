e72c6bfecb04cf804e08fbbeb08c4301e391f705
Kelvem Loubach <kelvemloubach@outlook.com>
2022-12-07 20:49:28 -0300

    Alterei texto do modal

--- app.js
+++ app.js
@@ -1,35 +1,31 @@
 
-const calculate = () =>{
+const calculate = () => {
     let age = document.getElementById('age').value;
     let height = document.getElementById('height').value;
     let weight = document.getElementById('weight').value;
-    let imc = weight/(height*height);
-   return imc;
+    let imc = weight / (height * height);
+    return imc;
 };
 
-
-
-const showModal = () =>{
-
+const showModal = () => {
     let imc = calculate();
-    if(imc < 18.5){
+    if (imc < 18.5) {
         document.getElementById('exampleModalLabel').innerHTML = 'Está abaixo do normal';
         document.getElementById('informations').innerHTML = `Seu índice de massa corporal é ${imc}!
         Experimente comer mais um pouco.`;
         $('#showModal1').modal('show');
-}if(imc > 18.5 && imc < 24.9){
-    document.getElementById('exampleModalLabel').innerHTML = 'Está normal!';
+    } if (imc > 18.5 && imc < 24.9) {
+        document.getElementById('exampleModalLabel').innerHTML = 'Está normal!';
         document.getElementById('informations').innerHTML = `Seu índice de massa corporal é ${imc}!
         Experimente comer mais um pouco.`;
         $('#showModal1').modal('show');
-}if(imc>25 && imc<29.9){
-    document.getElementById('exampleModalLabel').innerHTML = 'Está acima do normal!';
+    } if (imc > 25 && imc < 29.9) {
+        document.getElementById('exampleModalLabel').innerHTML = 'Está acima do normal!';
         document.getElementById('informations').innerHTML = `Seu índice de massa corporal é ${imc}!
-        Experimente fechar a boca carai!.`;
+        Experimente comer um pouco menos!.`;
         $('#showModal1').modal('show');
-}
-
- };
+    }
+};
 
 
 
