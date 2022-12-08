6d57e1f5aad29d0b69085f7fbd275f816f8fbc52
Kelvem Loubach <kelvemloubach@outlook.com>
2022-12-07 23:24:34 -0300

    Altereção da interface e código javaScript

--- app.js
+++ app.js
@@ -8,24 +8,33 @@ const calculate = () => {
 };
 
 const showModal = () => {
-    let imc = calculate();
+    const imc1 = calculate();
+    const imc = imc1.toFixed(2);
     if (imc < 18.5) {
         document.getElementById('exampleModalLabel').innerHTML = 'Está abaixo do normal';
-        document.getElementById('informations').innerHTML = `Seu índice de massa corporal é ${imc}!
+        document.getElementById('informations').innerHTML = `Seu índice de massa corporal é ${imc}%!
         Experimente comer mais um pouco.`;
         $('#showModal1').modal('show');
     } if (imc > 18.5 && imc < 24.9) {
         document.getElementById('exampleModalLabel').innerHTML = 'Está normal!';
-        document.getElementById('informations').innerHTML = `Seu índice de massa corporal é ${imc}!
+        document.getElementById('informations').innerHTML = `Seu índice de massa corporal é ${imc}%!
         Experimente comer mais um pouco.`;
         $('#showModal1').modal('show');
     } if (imc > 25 && imc < 29.9) {
         document.getElementById('exampleModalLabel').innerHTML = 'Está acima do normal!';
-        document.getElementById('informations').innerHTML = `Seu índice de massa corporal é ${imc}!
-        Experimente comer um pouco menos!.`;
+        document.getElementById('informations').innerHTML = `Seu índice de massa corporal é ${imc}%!
+        Experimente comer um pouco menos!`;
         $('#showModal1').modal('show');
-    }
+    };
 };
 
-
+const display = () => {
+    let width = window.innerWidth;
+    if (width < 576) {
+        document.getElementById('controlWindow').className = 'container mt-5';
+    } else {
+        document.getElementById('controlWindow').className = 'col-8 mt-5 mb-5';
+    };
+    console.log(width);
+};
 
